8fc020c52aa7da4dba07512241532e6f782d9f4f
walid <walid.sa3d@gmail.com>
2015-09-14 21:10:22 +0100

    update setup.py 	new file:   HISTORY 	new file:   MANIFEST.in 	new file:   requirements.txt 	new file:   setup.cfg 	modified:   setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,28 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @Author: walid
-# @Date:   2015-08-26 13:29:39
-# @Last Modified by:   walid
-# @Last Modified time: 2015-09-04 00:48:54
+from setuptools import find_packages
+from setuptools import setup
+
+try:
+    from pypandoc import convert
+    read_md = lambda f: convert(f, 'rst')
+except ImportError:
+    print("warning: pypandoc module not found, could not convert Markdown to RST")
+    read_md = lambda f: open(f, 'r').read()
+requires=[i.strip() for i in open("requirements.txt").readlines()]
+
+setup(
+    name='videoscene',
+    version="1.0.0",
+    description='Parse scene release tags',
+    long_description=read_md('README.md'),
+    author='Walid Saad',
+    author_email='walid.sa3d@gmail.com',
+    url='http://github.com/walidsa3d/videoscene',
+    packages=find_packages(),
+    package_data={'': ['LICENSE']},
+    include_package_data=True,
+    install_requires=requires,
+    test_suite="tests",
+    license="MIT",
+    zip_safe=False,
+
+)
