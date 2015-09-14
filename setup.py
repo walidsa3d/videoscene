ca973d8aa8d3668e20329746aefc25a328168676
walid <walid.sa3d@gmail.com>
2015-09-14 21:43:44 +0100

    fixed setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ requires=[i.strip() for i in open("requirements.txt").readlines()]
 
 setup(
     name='videoscene',
-    version="1.0.0",
+    version="0.1.0",
     description='Parse scene release tags',
     long_description=read_md('README.md'),
     author='Walid Saad',
