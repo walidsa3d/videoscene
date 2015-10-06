4cb44e8d018c63e8a63bb12db249d27ff1976547
walid <walid.sa3d@gmail.com>
2015-10-06 12:13:20 +0100

    fix setup.cfg, formatting

--- videoscene/scenedata.py
+++ videoscene/scenedata.py
@@ -1,30 +1,25 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# @Author: walid
-# @Date:   2015-08-27 10:10:19
-# @Last Modified by:   walid
-# @Last Modified time: 2015-08-27 11:05:43
 from collections import defaultdict
 
-definitions=defaultdict(lambda:"Unknown",{
-	'cam': 'a theater rip usually done with a digital video camera',
-	'ts': 'A theater rip done with digital video camera with an external audio source (most likely an audio jack in the chair for hard of hearing people)',
-	'tc': 'A telecine machine copies the film digitally from the reels',
-	'dvdscr': ' transferred off a DVD sent to retail stores for promotional use',
-	'retail': '',
-	'hdtv':'Digital recording from high definition source',
-	'sdtv':'',
-	'dvd-r':'',
-	'proper':'',
-	'limited':'',
-	'stv':'',
-	'dc':'',
-	'extended':'',
-	'remastered':'',
-	'unrated':'',
-	'repack':'',
-	'dubbed':'',
-	'dvdrip':'A copy of the final released DVD',
-	'vhsrip':'Transferred off a retail VHS'
+definitions = defaultdict(lambda: "Unknown", {
+    'cam': 'a theater rip usually done with a digital video camera',
+    'ts': 'A theater rip done with digital video camera with an external audio source (most likely an audio jack in the chair for hard of hearing people)',
+    'tc': 'A telecine machine copies the film digitally from the reels',
+    'dvdscr': ' transferred off a DVD sent to retail stores for promotional use',
+    'retail': '',
+    'hdtv': 'Digital recording from high definition source',
+    'sdtv': '',
+    'dvd-r': '',
+    'proper': '',
+    'limited': '',
+    'stv': '',
+    'dc': '',
+    'extended': '',
+    'remastered': '',
+    'unrated': '',
+    'repack': '',
+    'dubbed': '',
+    'dvdrip': 'A copy of the final released DVD',
+    'vhsrip': 'Transferred off a retail VHS'
 }
 )
