c0e7157830ec5f3f5e5bd112ca59f3c1018f890e
walid <walid.sa3d@gmail.com>
2015-11-08 21:52:11 +0100

    fix tests

--- tests/test_parse.py
+++ tests/test_parse.py
@@ -11,4 +11,4 @@ class CoreTest(unittest.TestCase):
         self.assertEqual(parsed['title'], 'Southpaw')
         self.assertEqual(parsed['year'], '2015')
         self.assertEqual(parsed['screenSize'], '720p')
-        self.assertEqual(parsed['rip'], 'BluRay')
+        self.assertEqual(parsed['quality'], 'BluRay')
